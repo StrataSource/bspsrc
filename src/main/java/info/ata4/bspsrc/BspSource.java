dece47b76a974b372e34d261393f276bea4cca47
rio <2745352+jason-e@users.noreply.github.com>
2023-08-08 03:34:21 -0400

    Strata temp version

--- src/main/java/info/ata4/bspsrc/BspSource.java
+++ src/main/java/info/ata4/bspsrc/BspSource.java
@@ -42,7 +42,7 @@ public class BspSource implements Runnable {
 
     private static final Logger L = LogUtils.getLogger();
 
-    public static final String VERSION = "1.4.3";
+    public static final String VERSION = "1.4.3-Strata";
 
     private final BspSourceConfig config;
 
